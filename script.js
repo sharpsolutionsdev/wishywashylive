9f9494240d8179d445a6ee2893a49be066b36799
sharpsolutionsdev <sharpsolutionsdev@gmail.com>
2025-04-09 12:02:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+  document.querySelector('.book-now-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('https://calendly.com/sharpsolutionsdev/30min').scrollIntoView({ behavior: 'smooth' });
+  });
